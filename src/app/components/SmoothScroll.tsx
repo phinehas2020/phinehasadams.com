06733d6aa873974b1a3bf2cdf534a5ff6aaf4773
Phinehas Adams <phinehasmadams@icloud.com>
2025-12-11 16:26:05 -0600

    Fix Lenis ReactNode type mismatch

--- src/app/components/SmoothScroll.tsx
+++ src/app/components/SmoothScroll.tsx
@@ -2,10 +2,12 @@
 
 import { ReactLenis } from '@studio-freight/react-lenis';
 
+type ReactLenisChildren = Parameters<typeof ReactLenis>[0]['children'];
+
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
     return (
         <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
-            {children}
+            {children as unknown as ReactLenisChildren}
         </ReactLenis>
     );
 }
